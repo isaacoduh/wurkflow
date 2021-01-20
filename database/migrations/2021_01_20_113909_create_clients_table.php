5449b99e35284574adaabf4d5c0effdcf41068bb
Isaac Oduh <ufuomaoduh@gmail.com>
2021-01-20 12:47:13 +0100

    Create user and client migrations and models

---
+++ database/migrations/2021_01_20_113909_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('office_address')->nullable();
+            $table->string('contact_name')->nullable();
+            $table->text('contact_office')->nullable();
+            $table->text('contact_email')->nullable();
+            $table->enum('type', ['individual', 'organization']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
