ff68da827ac2b9ae54d8483b7ff3137edae84c90
Isaac Oduh <ufuomaoduh@gmail.com>
2021-01-20 16:00:43 +0100

    Add client list component

---
+++ resources/js/components/ClientList.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <h2 class="text-center p-2 text-white bg-primary mt-5">Clients</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">S/N</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Contact Person</th>
+                    <th scope="col">Contact Email</th>
+                    <th class="col">Action</th>
+                </tr>
+            </thead>
+            <tbody v-for="client in clients" :key="client.id">
+                <tr>
+                    <th scope="row">{{ client.id }}</th>
+                    <td>{{ client.name }}</td>
+                    <td>{{ client.email }}</td>
+                    <td>{{ client.contact_name }}</td>
+                    <td>{{ client.contact_email }}</td>
+                    <td><button class="btn btn-danger btn-sm"></button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Client",
+    created() {
+        this.loadData();
+    },
+    methods: {
+        loadData() {
+            let url = this.url + "/api/clients";
+            this.axios.get(url).then(response => {
+                this.clients = response.data;
+                console.log(this.clients);
+            });
+        }
+    },
+    mounted() {},
+    data() {
+        return {
+            url: document.head.querySelector('meta[name="url"]').content,
+            clients: []
+        };
+    }
+};
+</script>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <h2 class="head-two">Landing Page</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+};
+</script>
+
+<style scoped>
+.head-two {
+    text-align: center;
+    margin-top: 90px;
+}
+</style>
